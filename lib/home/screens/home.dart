81949188aabc78fe39e01b0be4283d414e4dc13d
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2022-03-25 17:13:49 +0530

    created package

--- lib/home/screens/home.dart
+++ lib/home/screens/home.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_test/authentication/services/database_service.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,26 +8,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String name = '';
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
-  void getData() async {
-    name = (await DatabaseService().getUser(widget.email))!;
-    setState(() {});
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Center(
-        child: Text('Welcome $name!'),
+      body: const Center(
+        child: Text('Welcome !'),
       ),
     );
   }
